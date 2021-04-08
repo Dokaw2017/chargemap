f94c95d271331d761a449cde001b326847121750
Nebi <mam10122011@yahoo.com>
2021-04-08 21:13:45 +0300

    fixed the routes

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/stations", require("./router"));
+
 db.on("connected", () => {
   app.listen(3000);
 });
